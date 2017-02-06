cf6e31b39769cb4b130314ab5d96d55bcb5ac6cf
MrXyfir <mr@xyfir.com>
2017-02-06 12:25:07 -0800

    Add controller: POST api/buttons/:button/fork

---
+++ controllers/buttons/fork.js
@@ -0,0 +1,46 @@
+const mysql = require('lib/mysql');
+
+/*
+  POST api/buttons/:button/fork
+  RETURN
+    { error: boolean, message?: string, id?: number }
+  DESCRIPTION
+    Creates a copy of the button under requesting user
+*/
+module.exports = function(req, res) {
+
+  const db = new mysql();
+
+  db.getConnection()
+    .then(() => {
+      const sql = `
+        INSERT INTO buttons
+          (
+            user_id, name, description, is_listed, uri_match, domains,
+            script, repository
+          )
+          SELECT
+            ?, name, description, is_listed, uri_match, domains,
+            script, repository
+          FROM buttons
+          WHERE id = ?
+      `,
+      vars = [
+        req.session.uid,
+        req.params.button
+      ];
+
+      return db.query(sql, vars);
+    })
+    .then(result => {
+      if (!result.insertId) throw 'Could not fork button';
+
+      db.release();
+      res.json({ error: false, id: result.insertId });
+    })
+    .catch(err => {
+      db.release();
+      res.json({ error: true, message: err });
+    });
+
+};
