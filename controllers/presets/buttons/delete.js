5d87cd0d90f01205fc0861217a052684e7f5bc45
MrXyfir <mr@xyfir.com>
2017-02-06 12:16:40 -0800

    Add controller: DELETE api/presets/:preset/buttons/:button

---
+++ controllers/presets/buttons/delete.js
@@ -0,0 +1,45 @@
+const markPresetUpdated = require('lib/presets/mark-updated');
+const mysql = require('lib/mysql');
+
+/*
+  DELETE api/presets/:preset/buttons/:button
+  RETURN
+    { error: boolean, message?: string }
+  DESCRIPTION
+    Delete a button from user's preset
+*/
+module.exports = function(req, res) {
+
+  const db = new mysql();
+
+  db.getConnection()
+    .then(() => {
+      const sql = `
+        DELETE FROM preset_buttons
+        WHERE
+          button_id = ? AND preset_id IN (
+            SELECT id FROM presets WHERE id = ? AND user_id = ?
+          )
+      `,
+      vars = [
+        req.params.button,
+        req.params.preset, req.session.uid
+      ];
+
+      return db.query(sql, vars);
+    })
+    .then(result => {
+      if (!result.affectedRows) {
+        throw 'Could not delete button from preset';
+      }
+      else {
+        res.json({ error: false });
+        markPresetUpdated(db, req.params.preset);
+      }
+    })
+    .catch(err => {
+      db.release();
+      res.json({ error: true, message: err });
+    });
+
+};
