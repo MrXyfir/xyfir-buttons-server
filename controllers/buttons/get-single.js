0ded1291ac7c00ce8885a5161f746a55ea463af2
MrXyfir <mr@xyfir.com>
2017-02-06 12:24:11 -0800

    Add controller: GET api/buttons/:button

---
+++ controllers/buttons/get-single.js
@@ -0,0 +1,50 @@
+const getCreatorInfo = require('lib/users/get-creator-info');
+const mysql = require('lib/mysql');
+
+/*
+  GET api/buttons/:button
+  RETURN
+    {
+      id: number, name: string, uriMatch: string, repository: string,
+      isListed: boolean, description: string, domains: string,
+      created: date-string, updated: date-string, creator: {
+        id: number, name: string, reputation: number
+      }
+    }
+  DESCRIPTION
+    Returns the full data for a single button
+    Id is -1 if button could not be found
+*/
+module.exports = function(req, res) {
+
+  const db = new mysql();
+
+  db.getConnection()
+    .then(() => {
+      const sql = `
+        SELECT
+          id, user_id, name, uri_match AS uriMatch, repository, description,
+          domains, is_listed AS isListed, created, updated
+        FROM buttons WHERE id = ?
+      `,
+      vars = [
+        req.params.button
+      ];
+
+      return db.query(sql, vars);
+    })
+    .then(rows => {
+      if (!rows.length) throw 'Could not find button';
+
+      return getCreatorInfo(db, rows);
+    })
+    .then(rows => {
+      db.release();
+      res.json(rows[0]);
+    })
+    .catch(err => {
+      db.release();
+      res.json({ id: -1 });
+    });
+
+};
