6a721085894927e2f66d0089707d3b68de094f7e
MrXyfir <mr@xyfir.com>
2017-02-06 12:21:32 -0800

    Add controller: POST api/presets

---
+++ controllers/presets/create.js
@@ -0,0 +1,45 @@
+const validateAndBuild = require('lib/presets/validate-and-build');
+const mysql = require('lib/mysql');
+
+/*
+  POST api/presets
+  REQUIRED
+    name: string, uriMatch: string
+  OPTIONAL
+    description: string, domains: string, isListed: boolean
+  RETURN
+    { error: boolean, message?: string, id?: number }
+  DESCRIPTION
+    Creates a preset
+    If user is not logged in, the creator is anonymous
+*/
+module.exports = function(req, res) {
+
+  const db = new mysql();
+  let preset = req.body;
+
+  validateAndBuild(preset)
+    .then(p => {
+      preset = p;
+
+      return db.getConnection();
+    })
+    .then(() => {
+      preset.user_id = req.session.uid || 0;
+
+      const sql = `INSERT INTO presets SET ?`;
+
+      return db.query(sql, preset);
+    })
+    .then(result => {
+      if (!result.insertId) throw 'Could not create preset';
+
+      db.release();
+      res.json({ error: false, id: result.insertId });
+    })
+    .catch(err => {
+      db.release();
+      res.json({ error: true, message: err });
+    });
+
+};
