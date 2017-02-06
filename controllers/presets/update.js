f7a4f801f84956d7803d43ee9723e32ace3c83c9
MrXyfir <mr@xyfir.com>
2017-02-06 12:18:01 -0800

    Add controller: PUT api/presets/:preset

---
+++ controllers/presets/update.js
@@ -0,0 +1,56 @@
+const validateAndBuild = require('lib/presets/validate-and-build');
+const mysql = require('lib/mysql');
+
+/*
+  PUT api/presets/:preset
+  REQUIRED
+    name: string, uriMatch: string
+  OPTIONAL
+    description: string, domains: string, isListed: boolean
+  RETURN
+    { error: boolean, message?: string }
+  DESCRIPTION
+    Update an existing preset
+    presets created by anonymous users cannot be updated
+    Updates the `updated` column for the preset
+*/
+module.exports = function(req, res) {
+
+  const db = new mysql();
+  let preset = req.body;
+
+  validateAndBuild(preset)
+    .then(p => {
+      preset = p;
+
+      return db.getConnection();
+    })
+    .then(() => {
+      // Attempt the preset if it exists and user owns it
+      // Anonymously created presets cannot be updated
+      const sql = `
+        UPDATE presets SET
+          name = ?, description = ?, is_listed = ?, updated = NOW(),
+          domains = ?, uri_match = ?
+        WHERE id = ? AND user_id = ?
+      `,
+      vars = [
+        preset.name, preset.description, preset.is_listed,
+        preset.uri_match, preset.domains,
+        req.params.preset, req.session.uid
+      ];
+
+      return db.query(sql, vars);
+    })
+    .then(result => {
+      if (!result.affectedRows) throw 'Could not update preset';
+
+      db.release();
+      res.json({ error: false });
+    })
+    .catch(err => {
+      db.release();
+      res.json({ error: true, message: err });
+    });
+
+};
