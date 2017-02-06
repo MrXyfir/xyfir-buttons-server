6fd80939ef076863f0b6affb5097805eae4b6d59
MrXyfir <mr@xyfir.com>
2017-02-06 12:23:32 -0800

    Add controller: POST api/comments/:type/:id

---
+++ controllers/comments/create.js
@@ -0,0 +1,65 @@
+const mysql = require('lib/mysql');
+
+/*
+  POST api/comments/:type/:id
+  REQUIRED
+    comment: string
+  RETURN
+    { error: boolean, message?: string, id?: number }
+  DESCRIPTION
+    Allow user to comment on target object
+    Accepted targets: button, preset
+*/
+module.exports = function(req, res) {
+
+  const table = [
+    '', 'buttons', 'presets'
+  ][+req.params.type];
+
+  if (!table || !req.body.comment) {
+    res.json({ error: true, message: 'Invalid input' });
+    return;
+  }
+
+  const db = new mysql();
+  let sql = '', vars = [];
+
+  db.getConnection()
+    .then(() => {
+      sql = `
+        SELECT id FROM ${table} WHERE id = ?
+      `,
+      vars = [
+        req.params.id
+      ];
+
+      return db.query(sql, vars);
+    })
+    .then(rows => {
+      if (!rows.length) throw 'Could not find target';
+
+      sql = `
+        INSERT INTO comments
+          (target_type, target_id, user_id, comment)
+          VALUES (?, ?, ?, ?)
+      `,
+      vars = [
+        req.params.type, req.params.id, req.session.uid, req.body.comment
+      ];
+
+      return db.query(sql, vars);
+    })
+    .then(result => {
+      db.release();
+
+      if (!result.insertId)
+        throw 'Could not create comment';
+      else
+        res.json({ error: false, id: result.insertId });
+    })
+    .catch(err => {
+      db.release();
+      res.json({ error: true, message: err });
+    });
+
+};
