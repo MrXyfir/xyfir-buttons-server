53069942a2e3644ee5ee60b483f919b8763bf993
MrXyfir <mr@xyfir.com>
2017-02-06 12:22:05 -0800

    Add controller: GET api/comments/:type/:id

---
+++ controllers/comments/get.js
@@ -0,0 +1,78 @@
+const getCreatorInfo = require('lib/users/get-creator-info');
+const mysql = require('lib/mysql');
+
+/*
+  GET api/comments/:type/:id
+  REQUIRED
+    order: string
+      'votes|created'
+    direction: string
+      'asc|desc'
+  OPTIONAL
+    lastId: number
+  RETURN
+    {
+      comments: [{
+        id: number, created: date-string, comment: string, creator: {
+          id: number, name: string, reputation: number
+        }, 
+      }]
+    }
+  DESCRIPTION
+    Get comments for a target object
+    Returns empty comments array on error
+*/
+module.exports = function(req, res) {
+
+  if (
+    !['votes', 'created'].includes(req.query.order)
+    || !['asc', 'desc'].includes(req.query.direction)
+  ) {
+    res.json({ comments: [] });
+    return;
+  }
+
+  const db = new mysql();
+
+  db.getConnection()
+    .then(() => {
+      const sql = `
+        SELECT
+          id, user_id, created, comment, (
+            SELECT SUM(vote) FROM votes
+            WHERE target_id = ? AND target_type = ?
+          ) AS votes
+        FROM comments
+        WHERE
+          target_id = ? AND target_type = ? ${
+            req.query.lastId
+              ? `AND id ${
+                  req.query.direction == 'asc' ? '<' : '>'
+                } ${+req.query.lastId}`
+              : ''
+          }
+        ORDER BY ${req.query.order} ${req.query.direction}
+        LIMIT 26
+      `,
+      vars = [
+        req.params.id, req.params.type,
+        req.params.id, req.params.type
+      ];
+
+      return db.query(sql, vars);
+    })
+    .then(comments => {
+      if (!comments.length) throw 'No comments found';
+
+      return getCreatorInfo(db, comments);
+    })
+    .then(comments => {
+      db.release();
+      res.json({ comments });
+    })
+    .catch(err => {
+      db.release();
+      res.json({ comments: [] });
+    });
+
+};
