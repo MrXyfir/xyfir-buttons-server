a3b61e4e26940e271196cb0d9315dfa325768ac5
MrXyfir <mr@xyfir.com>
2017-03-08 11:58:15 -0800

    Use await. Fix bugs

--- controllers/buttons/download.js
+++ controllers/buttons/download.js
@@ -17,89 +17,83 @@ const mysql = require('lib/mysql');
   DESCRIPTION
     Download buttons with a newer version than provided
 */
-module.exports = function(req, res) {
+module.exports = async function(req, res) {
 
-  let buttons = [];
+  const db = new mysql();
+  let buttons = [], sql = '', vars = [];
   
   try {
     buttons = JSON.parse(req.query.buttons || '[]');
     
     if (!Array.isArray(buttons) || !buttons.length)
       throw 'Invalid or empty buttons array';
+    
+    await db.getConnection();
+
+    // Get updated column for all requested buttons
+    sql = `
+      SELECT id, updated FROM buttons WHERE id IN (?)
+    `,
+    vars = [
+      buttons.map(b => b.id)
+    ];
+
+    let rows = await db.query(sql, vars);
+
+    if (!rows.length) throw 'No buttons found';
+      
+    // Find buttons that need to be updated
+    const downloads = rows.map(b1 => {
+      const shouldDownload = buttons.findIndex(
+        b2 => b2.id == b1.id && b2.updated != b1.updated
+      ) > -1;
+
+      return shouldDownload ? b1.id : -1;
+    }).filter(b => b > -1);
+
+    if (!downloads.length) {
+      db.release();
+      res.json({
+        error: false, message: 'No buttons to update', buttons: []
+      }); return;
+    }
+
+    // Get full data for buttons being downloaded
+    sql = `
+      SELECT
+        id, name, user_id AS creator, description, domains, script, created,
+        updated, is_listed AS isListed, url_match AS urlMatch, updated,
+        repository, tooltip, styles, icon
+      FROM buttons WHERE id IN (?)
+    `,
+    vars = [
+      downloads
+    ];
+
+    rows = await db.query(sql, vars);
+
+    if (!rows.length) throw 'No buttons found';
+
+    res.json({ error: false, buttons: rows });
+
+    // Increment each button's downloads for the day
+    sql = `
+      INSERT INTO downloads
+        (target_type, target_id, day, downloads)
+      VALUES ${
+        rows.map(r =>
+          `('1', '${r.id}', CURDATE(), '1')`
+        ).join(', ')
+      }
+      ON DUPLICATE KEY UPDATE downloads = downloads + 1
+    `;
+
+    await db.query(sql);
+    db.release();
   }
   catch (e) {
     res.json({ error: true, message: e });
     return;
   }
-
-  const db = new mysql();
-  let sql = '', vars = [];
-
-  db.getConnection()
-    .then(() => {
-      // Get updated column for all requested buttons
-      sql = `
-        SELECT id, updated FROM buttons WHERE id IN (?)
-      `,
-      vars = [
-        buttons.map(b => b.id)
-      ];
-
-      return db.query(sql, vars);
-    })
-    .then(rows => {
-      if (!rows.length) throw 'No buttons found';
-      
-      // Find buttons that need to be updated
-      const downloads = rows.map(b1 => {
-        const shouldDownload = buttons.findIndex(
-          b2 => b2.id == b1.id && b2.updated != b1.updated
-        ) > -1;
-
-        return shouldDownload ? b1.id : -1;
-      }).filter(b => b > -1);
-
-      if (!downloads.length) throw 'No buttons to update';
-
-      // Get full data for buttons being downloaded
-      sql = `
-        SELECT
-          id, name, user_id AS creator, description, domains, script, created,
-          updated, is_listed AS isListed, url_match AS urlMatch, updated,
-          repository, tooltip, styles, icon
-        FROM buttons WHERE id IN (?)
-      `,
-      vars = [
-        downloads
-      ];
-
-      return db.query(sql, vars);
-    })
-    .then(rows => {
-      if (!rows.length) throw 'No buttons found';
-
-      res.json({ error: false, buttons: rows });
-
-      // Increment each button's downloads for the day
-      sql = `
-        INSERT INTO downloads
-          (target_type, target_id, day, downloads)
-        VALUES ${
-          rows.map(r =>
-            `('1', '${r.id}', CURDATE(), '1')`
-          ).join(', ')
-        }
-        ON DUPLICATE KEY UPDATE downloads = downloads + 1
-      `;
-
-      return db.query(sql);
-    })
-    .then(result => {
-      db.release();
-    })
-    .catch(err => {
-      db.release();
-      res.json({ error: true, message: err });
-    });
 
 };
--- controllers/presets/download.js
+++ controllers/presets/download.js
@@ -19,117 +19,112 @@ const mysql = require('lib/mysql');
   DESCRIPTION
     Download presets with a newer version than provided
 */
-module.exports = function(req, res) {
+module.exports = async function(req, res) {
 
-  let presets = [];
+  const db = new mysql();
+  let presets = [], response = { presets: [] }, downloads = [];
+  let sql = '', vars = [];
   
   try {
     presets = JSON.parse(req.query.presets || '[]');
 
     if (!Array.isArray(presets) || !presets.length)
       throw 'Invalid or empty presets array';
+    
+    await db.getConnection();
+
+    // Get updated column for all requested presets
+    sql = `
+      SELECT id, updated FROM presets WHERE id IN (?)
+    `,
+    vars = [
+      presets.map(b => b.id)
+    ];
+
+    let rows = await db.query(sql, vars);
+
+    if (!rows.length) throw 'No presets found';
+
+    // Find presets that need to be updated
+    downloads = rows.map(p1 => {
+      const shouldDownload = presets.findIndex(
+        p2 => p2.id == p1.id && p2.updated != p1.updated
+      ) > -1;
+
+      return shouldDownload ? p1.id : -1;
+    }).filter(p => p > -1);
+
+    if (!downloads.length) {
+      db.release();
+      res.json({
+        error: false, message: 'No presets to update', presets: []
+      }); return;
+    }
+
+    // Get full data for presets being downloaded
+    sql = `
+      SELECT
+        id, user_id AS creator, name, description, domains, created,
+        is_listed AS isListed, url_match AS urlMatch, updated
+      FROM presets WHERE id IN (?)
+    `,
+    vars = [
+      downloads
+    ];
+
+    rows = await db.query(sql, vars);
+
+    if (!rows.length) throw 'No presets found';
+
+    downloads = rows.map(r => r.id),
+    response.presets = rows,
+    response.presets = response.presets.map(p => {
+      p.buttons = [];
+      return p;
+    });
+
+    sql = `
+      SELECT
+        preset_id, button_id AS id, size, position, styles
+      FROM preset_buttons WHERE preset_id IN (?)
+    `,
+    vars = [
+      downloads
+    ];
+
+    rows = await db.query(sql, vars);
+
+    // Add buttons to response.presets[i].buttons
+    rows.forEach(r => {
+      for (let i = 0; i < response.presets.length; i++) {
+        if (r.preset_id == response.presets[i].id) {
+          delete r.preset_id;
+          response.presets[i].buttons.push(r);
+          break;
+        }
+      }
+    });
+
+    // Increment each preset's downloads for the day
+    sql = `
+      INSERT INTO downloads
+        (target_type, target_id, day, downloads)
+      VALUES ${
+        downloads.map(id =>
+          `('2', '${id}', CURDATE(), '1')`
+        ).join(', ')
+      }
+      ON DUPLICATE KEY UPDATE downloads = downloads + 1
+    `;
+
+    await db.query(sql);
+    db.release();
+    
+    res.json(response);
   }
-  catch (e) {
-    res.json({ error: false, message: e });
+  catch (err) {
+    res.json({ error: false, message: err });
     return;
   }
-
-  const db = new mysql();
-  let sql = '', vars = [], response = { presets: [] }, downloads = [];
-
-  db.getConnection()
-    .then(() => {
-      // Get updated column for all requested presets
-      sql = `
-        SELECT id, updated FROM presets WHERE id IN (?)
-      `,
-      vars = [
-        presets.map(b => b.id)
-      ];
-
-      return db.query(sql, vars);
-    })
-    .then(rows => {
-      if (!rows.length) throw 'No presets found';
-
-      // Find presets that need to be updated
-      downloads = rows.map(p1 => {
-        const shouldDownload = presets.findIndex(
-          p2 => p2.id == p1.id && p2.updated != p1.updated
-        ) > -1;
-
-        return shouldDownload ? p1.id : -1;
-      }).filter(p => p > -1);
-
-      if (!downloads.length) throw 'No presets to update';
-
-      // Get full data for presets being downloaded
-      sql = `
-        SELECT
-          id, user_id AS creator, name, description, domains, created,
-          is_listed AS isListed, url_match AS urlMatch, updated
-        FROM presets WHERE id IN (?)
-      `,
-      vars = [
-        downloads
-      ];
-
-      return db.query(sql, vars);
-    })
-    .then(rows => {
-      if (!rows.length) throw 'No presets found';
-
-      downloads = rows.map(r => r.id),
-      response.presets = rows,
-      response.presets = response.presets.map(p => {
-        p.buttons = [];
-        return p;
-      });
-
-      sql = `
-        SELECT
-          preset_id, button_id AS id, size, position, styles
-        FROM preset_buttons WHERE preset_id IN (?)
-      `,
-      vars = [
-        downloads
-      ];
-
-      return db.query(sql, vars);
-    })
-    .then(rows => {
-      // Add buttons to response.presets[i].buttons
-      rows.forEach(r => {
-        for (let i = 0; i < response.presets.length; i++) {
-          if (r.preset_id == response.presets[i].id) {
-            delete r.preset_id;
-            response.presets[i].buttons.push(r);
-            break;
-          }
-        }
-      });
-
-      // Increment each preset's downloads for the day
-      sql = `
-        INSERT INTO downloads
-          (target_type, target_id, day, downloads)
-        VALUES ${
-          downloads.map(id =>
-            `('2', '${id}', CURDATE(), '1')`
-          ).join(', ')
-        }
-        ON DUPLICATE KEY UPDATE downloads = downloads + 1
-      `;
-
-      return db.query(sql);
-    })
-    .then(result => {
-      db.release();
-      res.json(response)
-    })
-    .catch(err => {
-      db.release();
-      res.json({ error: false, message: err });
-    });
 
 };
